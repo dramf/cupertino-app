1eba4d11f64e535d31404bfd78a9f546769d26d7
AMF <work@afdesk.com>
2021-05-11 18:40:32 +0600

    Add basic widget and styles

---
+++ lib/styles.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class Styles {
+  static const TextStyle productrowItemName = TextStyle(
+    color: Color.fromRGBO(0, 0, 0, 0.8),
+    fontSize: 18,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle productRowTotal = TextStyle(
+    color: Color.fromRGBO(0, 0, 0, 0.8),
+    fontSize: 18,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.bold,
+  );
+}
