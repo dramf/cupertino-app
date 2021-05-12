41cc4e98a0d4c50fabeb44aceef470bf42340724
AMF <work@afdesk.com>
2021-05-12 18:12:08 +0600

    Add list products for sale

--- lib/product_list_tab.dart
+++ lib/product_list_tab.dart
@@ -1,3 +1,4 @@
+import 'package:cupertino_app/product_row_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 
@@ -12,12 +13,30 @@ class _ProductListTabState extends State<ProductListTab> {
   @override
   Widget build(BuildContext context) {
     return Consumer<AppStateModel>(
-        builder: (context, model, child) => const CustomScrollView(
-              slivers: [
-                CupertinoSliverNavigationBar(
-                  largeTitle: Text('Cupertino Store'),
-                ),
-              ],
-            ));
+      builder: (context, model, child) {
+        final products = model.getProducts();
+
+        return CustomScrollView(
+          semanticChildCount: products.length,
+          slivers: [
+            CupertinoSliverNavigationBar(
+              largeTitle: Text('Cupertino Store'),
+            ),
+            SliverSafeArea(
+              sliver: SliverList(
+                delegate: SliverChildBuilderDelegate((context, index) {
+                  if (index < products.length) {
+                    return ProductRowItem(
+                      product: products[index],
+                      lastItem: index == products.length - 1,
+                    );
+                  }
+                }),
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
---
+++ lib/product_row_item.dart
@@ -0,0 +1,95 @@
+import 'package:cupertino_app/model/app_state_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+
+import 'model/product.dart';
+import 'styles.dart';
+
+class ProductRowItem extends StatelessWidget {
+  final Product product;
+  final bool lastItem;
+
+  const ProductRowItem({
+    required this.product,
+    required this.lastItem,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final row = SafeArea(
+      top: false,
+      bottom: false,
+      minimum: const EdgeInsets.only(
+        left: 16,
+        top: 8,
+        right: 8,
+        bottom: 8,
+      ),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(4),
+            child: Image.asset(
+              product.assetName,
+              package: product.assetPackage,
+              fit: BoxFit.cover,
+              width: 76,
+              height: 76,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    product.name,
+                    style: Styles.productRowItemName,
+                  ),
+                  const Padding(padding: EdgeInsets.only(top: 8)),
+                  Text(
+                    '\$${product.price}',
+                    style: Styles.productRowItemPrice,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          CupertinoButton(
+            padding: EdgeInsets.zero,
+            onPressed: () {
+              final model = Provider.of<AppStateModel>(context, listen: false);
+              model.addProductToCart(product.id);
+            },
+            child: const Icon(
+              CupertinoIcons.plus_circled,
+              semanticLabel: 'Add',
+            ),
+          ),
+        ],
+      ),
+    );
+
+    if (lastItem) {
+      return row;
+    }
+
+    return Column(
+      children: [
+        row,
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 100,
+            right: 16,
+          ),
+          child: Container(
+            height: 1,
+            color: Styles.productRowDivider,
+          ),
+        )
+      ],
+    );
+  }
+}
